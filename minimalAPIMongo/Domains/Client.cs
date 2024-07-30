9b5cef4b9ab33f4ad021c0cdc09f8abf0f29a895
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-07-30 10:57:08 -0300

    Criado Domain de Client

--- minimalAPIMongo/Domains/Client.cs
+++ minimalAPIMongo/Domains/Client.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
 
 namespace minimalAPIMongo.Domains
 {
@@ -9,14 +10,18 @@ namespace minimalAPIMongo.Domains
         [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)] // O BsonElementatributo é mapeado para o nome do campo BSON. O BsonRepresentationatributo mapeia um tipo C# para um tipo BSON específico.
         public string? Id { get; set; }    
         [BsonElement("cpf")]// O BsonElementatributo é mapeado para o nome do campo BSON.
-        public string Cpf { get; set;}
+        public string? Cpf { get; set;}
         [BsonElement("phone")]// O BsonElementatributo é mapeado para o nome do campo BSON.
-        public string Phone { get; set;}
+        public string? Phone { get; set;}
         [BsonElement("adress")]// O BsonElementatributo é mapeado para o nome do campo BSON.
-        public string Adress { get; set; }
+        public string? Adress { get; set; }
 
+        //[JsonIgnore] // Notation para ignorar o dado abaixo no Json (Caso queira precise cadastrar a propriedae abaixo, necessário retirar o mesmo).
         [BsonElement("userId")]// O BsonElementatributo é mapeado para o nome do campo BSON.
-        public ObjectId UserId { get; set; }
+        public string? UserId { get; set; }
+
+        // Referencia para que consiga exibir o Usuario quando ver Cliente.
+        public User? User { get; set; }
 
         // As instruções abaixo permiti adicionar mais propriedades ao Obj que não estão deifinas no escopo acima.
         public Dictionary<string, string> AdditionalAtributes { get; set; }
