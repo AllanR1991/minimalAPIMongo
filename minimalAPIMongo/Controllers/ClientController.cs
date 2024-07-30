45271856601200ec587268bbe746b9dc07d79b7c
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-07-30 10:56:56 -0300

    Criado Controllers de Client e User

---
+++ minimalAPIMongo/Controllers/ClientController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using minimalAPIMongo.Domains;
+using minimalAPIMongo.Services;
+using MongoDB.Driver;
+using System;
+
+namespace minimalAPIMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        /// <summary>
+        /// Armazena os dados de acesso da collection
+        /// </summary>
+        private readonly IMongoCollection<Client> _client;
+        private readonly IMongoCollection<User> _user;
+
+        /// <summary>
+        /// Construtor que recebe como dependencia o objeto da classe Mongo DbService
+        /// </summary>
+        /// <param name="mongoDbService">Objeto da classe MongoDbService</param>
+        public ClientController(MongoDbService mongoDbService)
+        {
+            // Obtem a collection "client"
+            _client = mongoDbService.GetDatabase.GetCollection<Client>("client");
+            _user = mongoDbService.GetDatabase.GetCollection<User>("user");
+        }
+
+        [HttpGet]
+        // Task para atividade assincronas, ActionResult para retornar um status code,
+        public async Task<ActionResult<List<Client>>> Get()
+        {
+            try
+            {   
+                var clients = await _client.Find(FilterDefinition<Client>.Empty).ToListAsync();
+                return Ok(clients);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> NewItem(Client newProduct)
+        {
+            try
+            {
+                await _client.InsertOneAsync(newProduct);
+                return StatusCode(201, newProduct);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+                throw;
+            }
+        }
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Client>> Get(string id)
+        {
+            try
+            {
+                var cliente = await _client.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+                if (cliente is null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(cliente);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Client clienteAlterate)
+        {
+            try
+            {
+                var cliente = await _client.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+                if (cliente is null)
+                {
+                    return NotFound();
+                }
+
+                clienteAlterate.Id = cliente.Id;
+
+                await _client.ReplaceOneAsync(x => x.Id == id, clienteAlterate);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            try
+            {
+                var cliente = await _client.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+                if (cliente is null)
+                {
+                    return NotFound();
+                }
+
+                await _client.DeleteOneAsync(x => x.Id == id);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+                throw;
+            }
+        }
+
+    }
+}
---
+++ minimalAPIMongo/Controllers/UserController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using minimalAPIMongo.Domains;
+using minimalAPIMongo.Services;
+using MongoDB.Driver;
+
+namespace minimalAPIMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMongoCollection<User> _user;
+
+        public UserController(MongoDbService mongoDbService)
+        {
+            _user = mongoDbService.GetDatabase.GetCollection<User>("user");
+        }
+
+        [HttpGet]
+        // Task para atividade assincronas, ActionResult para retornar um status code,
+        public async Task<ActionResult<List<User>>> Get()
+        {
+            try
+            {
+                var users = await _user.Find(FilterDefinition<User>.Empty).ToListAsync();
+                return Ok(users);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> NewItem(User newProduct)
+        {
+            try
+            {
+                await _user.InsertOneAsync(newProduct);
+                return StatusCode(201, newProduct);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+                throw;
+            }
+        }
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<User>> Get(string id)
+        {
+            try
+            {
+                var user = await _user.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+                if (user is null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, User userAlterate)
+        {
+            try
+            {
+                var user = await _user.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+                if (user is null)
+                {
+                    return NotFound();
+                }
+
+                userAlterate.Id = user.Id;
+
+                await _user.ReplaceOneAsync(x => x.Id == id, userAlterate);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            try
+            {
+                var user = await _user.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+                if (user is null)
+                {
+                    return NotFound();
+                }
+
+                await _user.DeleteOneAsync(x => x.Id == id);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+                throw;
+            }
+        }
+    }
+}
